e60eac3b273285eedf39a87ee420bb4a5219c895
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-10-23 13:16:45 +0530

    fix: Update import paths in layout.js to use relative paths

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import AuthProvider from '@/lib/AuthProvider'
+import Navbar from '../components/Navbar'
+import AuthProvider from '../lib/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
